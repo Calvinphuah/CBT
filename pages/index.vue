b283ef7815d4f68c4c2ccd01d01e19d89934325c
Calvin Phuah <calvinphuah73@gmail.com>
2024-12-24 16:09:28 +0800

    feat: new card component

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="w-full mb-4 md:mb-0">
     <!-- Header Section -->
-    <h1 class="text-2xl font-bold mb-6 text-center"></h1>
+    <h1 class="mb-6 text-2xl font-bold text-center"></h1>
+
+    <div>
+      <CommonCard :cards="cards" />
+    </div>
 
     <!-- Responsive Cards Layout -->
-    <div
-      class="flex flex-col md:flex-row md:justify-center md:items-start gap-4 px-4 md:px-4"
+    <!-- <div
+      class="flex flex-col gap-4 px-4 md:flex-row md:justify-center md:items-start md:px-4"
     >
       <CommonCard
         v-for="(card, index) in cards"
@@ -14,31 +18,58 @@
         image="../../assets/images/meditate.png"
         :link="card.link"
       >
-        <p class="text-gray-600 mt-3">{{ card.description }}</p>
+        <p class="mt-3 text-gray-600">{{ card.description }}</p>
       </CommonCard>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script setup lang="ts">
+// const cards = [
+//   {
+//     title: "Cognitive Behavioural Therapy",
+//     image: "../../assets/images/meditate.png",
+//     description: "Engage in exercises to calm your thoughts and emotions.",
+//     link: "/cbt",
+//   },
+//   {
+//     title: "Gratitude",
+//     image: "../../assets/images/meditate.png",
+//     description: "Keep track of your daily thoughts and feelings.",
+//     link: "/gratitude",
+//   },
+//   {
+//     title: "Breathing Exercise",
+//     image: "../../assets/images/meditate.png",
+//     description: "Explore guided sessions for mindfulness and therapy.",
+//     link: "/breathing",
+//   },
+// ];
+
 const cards = [
   {
-    title: "Cognitive Behavioural Therapy",
-    image: "../../assets/images/meditate.png",
-    description: "Engage in exercises to calm your thoughts and emotions.",
+    title: "Your morning meditation",
+    description: "an exclusive meditation each day for you!",
+    bgColor: "bg-sky-200",
+    time: "8 min",
     link: "/cbt",
+    image: "https://cdn.pixabay.com/photo/2024/04/19/22/25/man-8707406_640.png",
   },
   {
-    title: "Gratitude",
-    image: "../../assets/images/meditate.png",
-    description: "Keep track of your daily thoughts and feelings.",
-    link: "/gratitude",
+    title: "Check-in & journal",
+    description: "how are you feeling today?",
+    bgColor: "bg-amber-100",
+    time: "2 min",
+    link: "/cbt",
+    image: "https://cdn.pixabay.com/photo/2024/04/19/22/25/man-8707406_640.png",
   },
   {
-    title: "Breathing Exercise",
-    image: "../../assets/images/meditate.png",
-    description: "Explore guided sessions for mindfulness and therapy.",
-    link: "/breathing",
+    title: "Surprise me!",
+    description: "brighten up your day with our surprise",
+    bgColor: "bg-green-100",
+    time: "5 min",
+    link: "/cbt",
+    image: "https://cdn.pixabay.com/photo/2024/04/19/22/25/man-8707406_640.png",
   },
 ];
 </script>
